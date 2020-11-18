b9b977530655633181226daea0ca5f74a19a7bc7
iamtreetop <trivta@gmail.com>
2020-11-18 11:00:18 -0800

    Add Edit Request Form

--- frontend/components/requests/edit_request_form.jsx
+++ frontend/components/requests/edit_request_form.jsx
@@ -5,6 +5,10 @@ class EditRequestForm extends React.Component {
     componentDidMount(){
         this.props.fetchRequest(this.props.match.params.requestId);
     }
+    
+    update(field) {
+        return (e) => this.setState({ [field]: e.currentTarget.value })
+    }
 
     render() {
         const { formType, request, updateRequest } = this.props;
@@ -12,7 +16,18 @@ class EditRequestForm extends React.Component {
         if (!request) return null;
         return (
             <div>
-                return null
+                <form action="">
+                    <input className="form-amount"
+                        type="text"
+                        placeholder="$0.00"
+                        onChange={this.update('amount')}
+                    />
+                    <input className="note-box"
+                        type="text"
+                        placeholder="Add a note"
+                        onChange={this.update('note')}
+                    />
+                </form>
             </div>
             // <RequestForm
             // formType={formType}
