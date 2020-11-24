c1be21b0ba47599ba0dfd802b1e001dd8c028c60
iamtreetop <trivta@gmail.com>
2020-11-23 16:09:43 -0800

    ???

--- frontend/components/dashboard/dashboard.jsx
+++ frontend/components/dashboard/dashboard.jsx
@@ -147,7 +147,10 @@ class Dashboard extends React.Component {
                                 </ul>
                             </div>
 
+<<<<<<< HEAD
                             {/* <div className="pals-container"> */}
+=======
+>>>>>>> ba0e31e... ???
                             {/* <div className="pals-container">
                                 <h3 className="pals-header">Send Again</h3>
                                 <ul className="pals-list">
